ffe22068d0211d5a7518caac43ee7e523b035586
Jesse Yang <jesseyang432@gmail.com>
2021-01-24 23:35:22 -0600

    constant 3 health mucls

--- jesserobot2/RobotPlayer.java
+++ jesserobot2/RobotPlayer.java
@@ -349,8 +349,8 @@ public strictfp class RobotPlayer{
             }
             for (int i = 0; i < 8; i++) {
                 Direction dir = directions[(((rc.getRoundNum() - 1)/2%8) + i)%8];
-                if (rc.canBuildRobot(RobotType.MUCKRAKER, dir, rc.getInfluence()/36)) {
-                    rc.buildRobot(RobotType.MUCKRAKER, dir, rc.getInfluence()/36);
+                if (rc.canBuildRobot(RobotType.MUCKRAKER, dir, 3)) {
+                    rc.buildRobot(RobotType.MUCKRAKER, dir, 3);
                     spawnedRobots.add(rc.senseRobotAtLocation(rc.getLocation().add(dir)).getID());
                     break;
                 }
